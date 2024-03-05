75ebdeaecbd5ad817d10552d2375593ead356ac6
HudsonMenezes <hudson.menezes@criaresistemas.com>
2024-03-05 17:03:26 -0300

    formulário de login iniciado, criando UserTextInput component

---
+++ components/UserTextInput.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";import React from "react";
+
+const UserTextInput = ({
+  placeholder,
+  isPass,
+  setStateValue,
+  setStateFunction,
+}) => {
+  return (
+    <View>
+      <Text>UserTextInput</Text>
+    </View>
+  );
+};
+
+export default UserTextInput;
---
+++ screens/LoginScreen.jsx
@@ -0,0 +1,47 @@
+import { View, Text, Image, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { BGImage, Logo } from "../assets";
+import { UserTextInput } from "../components";
+
+const LoginScreen = () => {
+  const screenWidth = Math.round(Dimensions.get("window").width);
+
+  const [email, setEmail] = useState("");
+
+  return (
+    <View className="flex-1 justify-start items-center">
+      <Image
+        source={BGImage}
+        resizeMode="cover"
+        className="h-96"
+        style={{ width: screenWidth }}
+      />
+
+      {/* Main View */}
+      <View className="w-full h-full bg-white rounded-tl-[90px] -mt-44 flex items-center justify-start py-6 px-6 space-y-6">
+        <Image source={Logo} className="w-16 h-16" resizeMode="contain" />
+
+        <Text className="py-2 text-primaryText text-xl font-semibold">
+          Bem vindo novamente!
+        </Text>
+
+        <View className="w-full flex items-center justify-center">
+          {/* Alert */}
+
+          {/* email */}
+          <UserTextInput
+            placeholder="Email"
+            isPass={false}
+            setStateValue={email}
+            setStateFunction={setEmail}
+          />
+          {/* senha */}
+
+          {/* botão de login */}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
